696403ad81761a7ba8846d5fc885fad69c9f8bbb
Ba <duongvanba.com@gmail.com>
2021-08-29 12:40:57 +0700

    Fix auto connect

--- index.ts
+++ index.ts
@@ -1,21 +1,104 @@
 import { Message, connect, Channel, Connection } from 'amqplib'
-import { Message as TransporterMessage, Transporter, ListenOptions, CallBackFunction, PublishOptions, sleep } from 'typescript-microservice'
+import { Message as TransporterMessage, Transporter, ListenOptions, CallBackFunction, PublishOptions } from 'typescript-microservice'
+import { ReplaySubject, fromEvent, merge, firstValueFrom, mergeMap } from 'rxjs'
 
 
 export class AmqpTransporter implements Transporter {
 
-    constructor(
-        private push_channel: Channel,
-        private listen_connection: Connection,
-        private listen_default_channel: Channel
-    ) { }
+    private push_connection: Connection
+    private listen_connection: Connection
+
+    private push_channel: Channel
+    private listen_default_channel: Channel
+    private listeners = new ReplaySubject<{
+        topic: string,
+        cb: CallBackFunction,
+        options: ListenOptions,
+        queue_name: string
+    }>(1000)
+
+    private constructor(
+        private url: string
+    ) {
+
+    }
+
+    private setup_listeners() {
+
+        return this
+            .listeners
+            .pipe(
+                mergeMap(async ({ cb, options, topic, queue_name }) => {
+                    const channel = await this.getChannel(options)
+                    const { queue } = await channel.assertQueue(queue_name, {
+                        autoDelete: true
+                    })
+                    await this.push_channel.assertExchange(topic, 'topic', { autoDelete: true })
+                    await channel.bindQueue(queue, topic, options.route || '#')
+                    await channel.consume(queue, async (msg: Message) => {
+                        const { content, properties: { timestamp, messageId, replyTo } } = msg
+                        const data: TransporterMessage = {
+                            content,
+                            created_time: timestamp,
+                            id: messageId,
+                            reply_to: replyTo,
+                            delivery_attempt: msg.properties.headers["x-death"]?.length || 0
+                        }
+                        await cb(data)
+                        channel.ack(msg)
+                    }, { noAck: false })
+                    return queue
+                }, 1)
+            )
+            .subscribe()
+    }
+
+    private async init() {
+        await new Promise(async (s, r) => {
+            try {
+                while (true) {
+
+                    // Init connection
+                    this.push_connection = await connect(this.url, { reconnectTimeInSeconds: 2, heartbeatIntervalInSeconds: 1 })
+                    this.listen_connection = await connect(this.url, { reconnectTimeInSeconds: 2, heartbeatIntervalInSeconds: 1 })
+                    this.push_channel = await this.push_connection.createChannel()
+                    this.listen_default_channel = await this.push_connection.createChannel()
+
+                    // Setup listeners
+                    const subcription = await this.setup_listeners()
+
+                    // Wait for error
+                    s(1)
+                    await firstValueFrom(
+                        merge(
+                            fromEvent(this.push_connection, 'error'),
+                            fromEvent(this.listen_connection, 'error'),
+                            fromEvent(this.push_channel, 'error'),
+                            fromEvent(this.listen_default_channel, 'error')
+                        )
+                    )
+                    console.log(`Error with rabbitmq, reconnect in 3s...`)
+                    subcription.unsubscribe()
+                    try {
+                        this.listen_default_channel.close()
+                        this.push_channel.close()
+                        this.listen_connection.close()
+                        this.push_connection.close()
+                    } catch (e) {
+                        console.log(e)
+                    }
+                    await new Promise(s => setTimeout(s, 3000))
+                }
+            } catch (e) {
+                r(e)
+            }
+        })
+    }
 
     static async init(url: string = process.env.AMQP_TRANSPORTER) {
-        const push_connection = await connect(url, { reconnectTimeInSeconds: 2, heartbeatIntervalInSeconds: 1 })
-        const listen_connection = await connect(url, { reconnectTimeInSeconds: 2, heartbeatIntervalInSeconds: 1 })
-        const push_channel = await push_connection.createChannel()
-        const listen_default_channel = await push_connection.createChannel()
-        return new this(push_channel, listen_connection, listen_default_channel)
+        const instance = new this(url)
+        await instance.init()
+        return instance
     }
 
     async publish(topic: string, data: Buffer, options: PublishOptions = {}) {
@@ -41,27 +124,12 @@ export class AmqpTransporter implements Transporter {
         return this.listen_default_channel
     }
 
-    async listen(topic: string, cb: CallBackFunction, options: ListenOptions = {}) {
 
-        const channel = await this.getChannel(options)
-        const { queue } = await channel.assertQueue(options.fanout ? '' : `${topic}${options.route || ''}`, {
-            autoDelete: true
-        })
-        await this.push_channel.assertExchange(topic, 'topic', { autoDelete: true })
-        await channel.bindQueue(queue, topic, options.route || '#')
-        await channel.consume(queue, async (msg: Message) => {
-            const { content, properties: { timestamp, messageId, replyTo } } = msg
-            const data: TransporterMessage = {
-                content,
-                created_time: timestamp,
-                id: messageId,
-                reply_to: replyTo,
-                delivery_attempt: msg.properties.headers["x-death"]?.length || 0
-            }
-            await cb(data)
-            channel.ack(msg)
-        }, { noAck: false })
-        return queue
+
+    async listen(topic: string, cb: CallBackFunction, options: ListenOptions = {}) {
+        const queue_name = options.fanout ? '' : `${topic}${options.route || ''}`
+        this.listeners.next({ topic, cb, queue_name, options })
+        return queue_name
     }
 
 }
